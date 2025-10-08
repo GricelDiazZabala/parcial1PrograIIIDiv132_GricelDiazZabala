3855a6b0fa542039c0724bcec01be2f51ff7dcec
Gricel Diaz Zabala <griceldiazzabala@gmail.com>
2025-10-08 09:25:31 -0300

    punto 1

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//Punto 1, creo un array de objetos con todas las frutas segun indica el pdf
+const frutas = [
+    {id: 1, nombre: "arandano", precio: 5000, ruta_img: "../img/arandano.jpg"},
+    {id: 2, nombre: "banana", precio: 1000, ruta_img: "../img/banana.jpg"},
+    {id: 3, nombre: "frambuesa", precio: 4000, ruta_img: "../img/frambuesa.jpg"},
+    {id: 4, nombre: "frutilla", precio: 3000, ruta_img: "../img/frutilla.jpg"},
+    {id: 5, nombre: "kiwi", precio: 2000, ruta_img: "../img/kiwi.jpg"},
+    {id: 6, nombre: "mandarina", precio: 800, ruta_img: "../img/mandarina.jpg"},
+    {id: 7, nombre: "manzana", precio: 1500, ruta_img: "../img/manzana.jpg"},
+    {id: 8, nombre: "naranja", precio: 9000, ruta_img: "../img/naranja.jpg"},
+    {id: 9, nombre: "pera", precio: 2500, ruta_img: "../img/pera.jpg"},
+    {id: 10, nombre: "anana", precio: 3000, ruta_img: "../img/anana.jpg"},
+    {id: 11, nombre: "pomelo-amarillo", precio: 2000, ruta_img: "../img/pomelo-amarillo.jpg"},
+    {id: 12, nombre: "pomelo-rojo", precio: 2000, ruta_img: "../img/pomelo-rojo.jpg"},
+    {id: 13, nombre: "sandia", precio: 10000, ruta_img: "../img/sandia.jpg"}
+];
